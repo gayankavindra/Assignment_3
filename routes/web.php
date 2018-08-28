1ae20e56fd3bf7eabd61e8d57e1910e557dc1502
BPavitran <36607071+BPavitran@users.noreply.github.com>
2018-08-28 22:12:09 +0530

    User Role Management

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth','admin']],function(){
+		Route::get('/admin',function(){
+			return view('admin');
+});
+});
